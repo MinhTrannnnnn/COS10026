c0d7deaaa4cdc63abc0b31992662c685f4431de7
Minh Tran <105227448@student.swin.edu.au>
2025-10-07 23:54:55 +1100

    lab09

---
+++ lab09/searchform.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Car Model</title>
+</head>
+<body>
+    <h2>Search for a Car Model</h2>
+    <form action="search_result.php" method="get">
+        <label for="model">Model:</label>
+        <input type="text" name="model" id="model" required>
+        <input type="submit" value="Search">
+    </form>
+</body>
+</html>
